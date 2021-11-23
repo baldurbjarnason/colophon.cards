c6463e0a5ca36e65cd892237ebbcdac2d4668a84
Baldur Bjarnason <baldur@rebus.foundation>
2021-11-23 14:33:37 +0000

    Make sure netlify picks up the redirects and such

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ module.exports = function (eleventyConfig) {
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("_headers");
 
   // Copy `css/fonts/` to `_site/css/fonts`
   // Keeps the same directory structure.
